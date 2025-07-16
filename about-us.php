cab8ceab0584c4a6133c4cfbe382d1fc7db1e4be
rj31portfolio <rjrajuali31@gmail.com>
2025-07-16 10:23:53 +0530

    make abot section

--- about-us.php
+++ about-us.php
@@ -34,7 +34,81 @@
 
 <div class="page-content">
 
-
+<section class="d-none">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-6 col-md-12">
+        <img class="img-fluid" src="images/about/04.jpg" alt="">
+      </div>
+      <div class="col-lg-6 col-md-12 mt-10 mt-lg-0 ps-lg-10">
+        <div class="theme-title">
+          <h6>About Us</h6>
+          <h2 class="text-anime-style">The Modern &amp; Largest Industrial Company</h2>
+          <div class="title-divider">
+            <span class="title-divider-separator"></span>
+          </div>
+          <p>Welcome to induztry, a leading industry innovator with a rich history of excellence, At the heart of the global landscape, the industry stands as a multidimensional force of progress, driving economies.</p>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="ht-progress-bar">
+              <div class="flex-grow-1">
+                <h4>Manufacturing</h4>
+                <div class="progress" data-value="85">
+                  <div class="progress-bar" style="width: 85%;">
+                    <div class="progress-parcent">
+                      <span>85</span>%
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="ht-progress-bar">
+              <div class="flex-grow-1">
+                <h4>Building Construction</h4>
+                <div class="progress" data-value="90">
+                  <div class="progress-bar" style="width: 90%;">
+                    <div class="progress-parcent">
+                      <span>90</span>%
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="ht-progress-bar">
+              <div class="flex-grow-1">
+                <h4>Industry</h4>
+                <div class="progress" data-value="70">
+                  <div class="progress-bar" style="width: 70%;">
+                    <div class="progress-parcent">
+                      <span>70</span>%
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 mt-6 mt-lg-0">
+            <ul class="list-unstyled list-icon style-1">
+              <li>
+                <i class="flaticon flaticon-check-mark-1"></i> Expertise and Experience
+              </li>
+              <li>
+                <i class="flaticon flaticon-check-mark-1"></i> Customer-Centic Approach
+              </li>
+              <li>
+                <i class="flaticon flaticon-check-mark-1"></i> Always Work With Energetic
+              </li>
+              <li>
+                <i class="flaticon flaticon-check-mark-1"></i> We are Award Winning Company
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 
 
