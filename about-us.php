6b6799cec65e2725d4d0e2a0588565632a87c085
rj31portfolio <rjrajuali31@gmail.com>
2025-07-16 09:51:08 +0530

    working on about page header and footer

---
+++ about-us.php
@@ -0,0 +1,27 @@
+<?php require_once('includes/config.php');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php require_once('includes/head.php');?>
+</head>
+
+<body>
+    <!-- page wrapper start -->
+
+<div class="page-wrapper">
+
+<!-- preloader start -->
+<?php require_once('includes/preloader.php');?>
+<!-- preloader end -->
+
+<!-- preloader end -->
+
+
+<!--header start-->
+<?php require_once('includes/header.php');?>
+<!--header end-->
+
+
+<!--footer start-->
+<?php require_once('includes/footer.php');?>
+
