ff493cfaf13e62c2830e44f01b84a9e92b874047
rj31portfolio <rjrajuali31@gmail.com>
2025-07-17 18:10:55 +0530

    make product page

--- products/C-Frame-Power-Press.php
+++ products/C-Frame-Power-Press.php
@@ -39,7 +39,53 @@
   </div>
 </section>
 
+<!--product start-->
 
+<section>
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-5 col-md-6">
+        <img class="img-fluid w-100" src="images/product/01.jpg" alt="" />
+      </div>
+      <div class="col-lg-7 col-md-6 mt-5 mt-md-0 ps-lg-5">
+        <div class="product-details">
+          <h3>C Frame Power Press</h3>
+          <div class="product-price my-4">
+            <span class="product-price text-black">
+              
+            <span class="text-theme">
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+            </span>
+          </div>
+          <ul class="list-unstyled mb-4">
+            <li class="mb-2">
+              <span class="text-black"> Availibility: </span> In Stock
+            </li>
+            <li>
+              <span class="text-black"> Categories :</span> Equipment
+            </li>
+          </ul>
+          <p>High-performance C Frame Power Press Machines ideal for precision metal stamping and forming. Durable design, efficient operation—perfect for industrial manufacturing.</p>
+         
+          <div class="d-flex align-items-center">
+            <div class="d-flex align-items-center">
+            </div>
+          </div>
+          <div class="product-link d-flex align-items-center mt-5">
+            <button class="themeht-btn primary-btn me-3" type="button"><span>Enquiry Now</span><i class="bi bi-person-lines-fill"></i></button>
+            <button class="themeht-btn dark-btn" type="button"><span>WhatsApp</span><i class="bi bi-whatsapp"></i></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--product end-->
 
 
 
