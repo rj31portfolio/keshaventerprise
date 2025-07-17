d3ef59ace6972a95c3de9c9482a9684274d119a2
rj31portfolio <rjrajuali31@gmail.com>
2025-07-17 17:23:05 +0530

    make c fram power press product

---
+++ products/C-Frame-Power-Press.php
@@ -0,0 +1,44 @@
+<?php require_once('../includes/config.php');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php require_once('../includes/head.php');?>
+</head>
+
+<body>
+
+<!-- page wrapper start -->
+
+<div class="page-wrapper">
+
+<!-- preloader start -->
+<?php require_once('../includes/preloader.php');?>
+<!-- preloader end -->
+
+<!-- preloader end -->
+
+
+<!--header start-->
+<?php require_once('../includes/header.php');?>
+<!--header end-->
+<section class="page-title" data-bg-img="images/bg/02.jpg" style="background-image: url(&quot;images/bg/02.jpg&quot;);">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <h1>
+          C Frame Power Press
+        </h1>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+</div>
+
+<!--body content end--> 
+
+<!--footer start-->
+<?php require_once('../includes/footer.php');?>
